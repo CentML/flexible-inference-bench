f34d5832a8071136e15132a1d6f0ce2bd9ad29e2
Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>
2024-04-14 07:17:19 +0000

    Update random test
    Signed-off-by: Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,19 +6,21 @@ import modular_inference_benchmark.engine.distributions as distributions
 
 def test_random_with_prefix_str():
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    distribution = distributions.UniformInt(10, 20)
+    distribution = distributions.UniformInt(10, 11)
     random_data_generator = data.Random.with_prefix_str(
-        prefix_str="Hello, world!", prefill_distribution=distribution, tokenizer=tokenizer
+        prefix_str="Hello world!", prefill_distribution=distribution, tokenizer=tokenizer
     )
     random_data = random_data_generator.generate_data(10)
-    assert len(random_data) == 10
+    lens = [len(tokenizer.encode(x)) for x in random_data]
+    assert lens == [10] * 10
 
 
 def test_random_with_prefix_len():
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    distribution = distributions.UniformInt(10, 20)
+    distribution = distributions.UniformInt(10, 11)
     random_data_generator = data.Random.with_prefix_len(
-        prefix_len=10, prefill_distribution=distribution, tokenizer=tokenizer
+        prefix_len=5, prefill_distribution=distribution, tokenizer=tokenizer
     )
     random_data = random_data_generator.generate_data(10)
-    assert len(random_data) == 10
+    lens = [len(tokenizer.encode(x)) for x in random_data]
+    assert lens == [10] * 10
