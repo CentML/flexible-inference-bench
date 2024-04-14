3355bfa266f68d48d31532de4773a0dd74e72db6
Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>
2024-04-14 06:15:42 +0000

    Added initial code
    Signed-off-by: Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>

---
+++ modular_inference_benchmark/engine/data.py
@@ -0,0 +1,194 @@
+import abc
+from typing import List
+import logging
+import json
+import random
+
+import transformers
+
+from modular_inference_benchmark.engine import distributions
+
+logger = logging.getLogger(__name__)
+
+
+def get_data_end(
+    data: List[int], tokenizer: transformers.PreTrainedTokenizer, idx: int, length: int, num_trials: int
+) -> int:
+    assert length >= 0 and idx >= 0
+    if length == 0:
+        return idx
+
+    idy = idx + length
+
+    def get_length(x: int, y: int) -> int:
+        return len(tokenizer.encode(tokenizer.decode(data[x:y])))
+
+    for _ in range(num_trials):
+        if get_length(idx, idy) == length:
+            break
+        if get_length(idx, idy) < length:
+            idy += 1
+        else:
+            idy -= 1  # Could potentially be stuck in a cycle if the length is not achievable
+            if idy < idx:
+                idy = idx
+                break
+
+    if get_length(idx, idy) != length:
+        logger.warning(f"Tried to achieve length {length} but failed. Achieved length {get_length(idx, idy)} instead")
+
+    return idy
+
+
+class Data(abc.ABC):
+    @abc.abstractmethod
+    def generate_data(self, size: int) -> List[str]:
+        pass
+
+
+class Textfile(Data):
+    def __init__(
+        self,
+        data: List[int],
+        prefix_str: str,
+        prefill_distribution: distributions.Distribution,
+        tokenizer: transformers.PreTrainedTokenizer,
+        num_trials: int,
+    ) -> None:
+        self.prefix_str = prefix_str
+        self.prefill_distribution = prefill_distribution
+        self.start_distribution = distributions.AdjustedUniformInt(0, len(data) - num_trials)
+        self.tokenizer = tokenizer
+        self.data = data
+        self.num_trials = num_trials
+
+    @classmethod
+    def with_prefix_str(
+        cls,
+        filename: str,
+        prefix_str: str,
+        prefill_distribution: distributions.Distribution,
+        tokenizer: transformers.PreTrainedTokenizer,
+        num_trials: int = 10,
+    ) -> "Textfile":
+        with open(filename) as f:
+            text = f.read()
+        data = tokenizer.encode(text)
+
+        return cls(data, prefix_str, prefill_distribution, tokenizer, num_trials)
+
+    @classmethod
+    def with_prefix_len(
+        cls,
+        filename: str,
+        prefix_len: int,
+        prefill_distribution: distributions.Distribution,
+        tokenizer: transformers.PreTrainedTokenizer,
+        num_trials: int = 10,
+    ) -> "Textfile":
+        with open(filename) as f:
+            text = f.read()
+        data = tokenizer.encode(text)
+
+        if prefix_len + num_trials >= len(data):
+            raise ValueError("Prefix length is too long")
+
+        prefix_end = get_data_end(data, tokenizer, 0, prefix_len, num_trials)  # prefix real length
+
+        prefix_str = tokenizer.decode(data[:prefix_end]) if prefix_end > 0 else ""
+
+        return cls(data[prefix_end:], prefix_str, prefill_distribution, tokenizer, num_trials)
+
+    def generate_data(self, size: int) -> List[str]:
+        # Can save memory by using a generator. However for performance we will use a list
+        input_data = []
+        lengths = self.prefill_distribution.generate_distribution(size)
+        starts = self.start_distribution.generate_distribution(lengths)
+        prefix_len = len(self.tokenizer.encode(self.prefix_str))
+
+        for i in range(size):
+            prompt_end = get_data_end(self.data, self.tokenizer, starts[i], lengths[i] - prefix_len, self.num_trials)
+            input_data.append(self.prefix_str + self.tokenizer.decode(self.data[starts[i] : prompt_end]))
+
+        return input_data
+
+
+class Random(Data):
+    def __init__(
+        self,
+        prefix_str: str,
+        prefill_distribution: distributions.Distribution,
+        token_distribution: distributions.Distribution,
+        tokenizer: transformers.PreTrainedTokenizer,
+        num_trials: int,
+    ) -> None:
+        self.tokenizer = tokenizer
+        self.prefill_distribution = prefill_distribution
+        self.token_distribution = token_distribution
+        self.prefix_str = prefix_str
+        self.num_trials = num_trials
+
+    @classmethod
+    def with_prefix_str(
+        cls,
+        prefix_str: str,
+        prefill_distribution: distributions.Distribution,
+        tokenizer: transformers.PreTrainedTokenizer,
+        num_trials: int = 10,
+    ) -> "Random":
+        token_distribution = distributions.UniformInt(0, len(tokenizer.get_vocab()))
+
+        return cls(prefix_str, prefill_distribution, token_distribution, tokenizer, num_trials)
+
+    @classmethod
+    def with_prefix_len(
+        cls,
+        prefix_len: int,
+        prefill_distribution: distributions.Distribution,
+        tokenizer: transformers.PreTrainedTokenizer,
+        num_trials: int = 10,
+    ) -> "Random":
+        token_distribution = distributions.UniformInt(0, len(tokenizer.get_vocab()))
+        data = list(token_distribution.generate_distribution(prefix_len + num_trials))
+        prefix_end = get_data_end(data, tokenizer, 0, prefix_len, num_trials)  # prefix real length
+        prefix_str = tokenizer.decode(data[:prefix_end]) if prefix_end > 0 else ""
+
+        return cls(prefix_str, prefill_distribution, token_distribution, tokenizer, num_trials)
+
+    def generate_data(self, size: int) -> List[str]:
+        input_data = []
+        lengths = self.prefill_distribution.generate_distribution(size)
+        prefix_len = len(self.tokenizer.encode(self.prefix_str))
+
+        for i in range(size):
+            data = list(self.token_distribution.generate_distribution(lengths[i] + self.num_trials))
+            prompt_end = get_data_end(data, self.tokenizer, 0, lengths[i] - prefix_len, self.num_trials)
+            input_data.append(self.prefix_str + self.tokenizer.decode(data[:prompt_end]))
+
+        return input_data
+
+
+class ShareGPT(Data):
+    def __init__(self, filename: str, tokenizer: transformers.PreTrainedTokenizer) -> None:
+        # From https://github.com/vllm-project/vllm/blob/v0.4.0.post1/benchmarks/benchmark_serving.py#L310
+
+        self.tokenizer = tokenizer
+
+        with open(filename) as f:
+            dataset = json.load(f)
+
+        dataset = [data for data in dataset if len(data["conversations"]) > 2]
+
+        dataset = [
+            (data["conversations"][0]["value"], data["conversations"][1]["value"])
+            for data in dataset
+            if (
+                len(tokenizer.encode(data["conversations"][0]["value"])) > 4
+                and len(tokenizer.encode(data["conversations"][1]["value"])) > 4
+            )
+        ]
+
+        self.data = dataset
+
+    def generate_data(self, size: int) -> List[str]:
+        return random.sample(self.data, size)
---
+++ modular_inference_benchmark/engine/distributions.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass
+import abc
+import logging
+from typing import Any
+
+import numpy as np
+import numpy.typing as npt
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Distribution(abc.ABC):
+    @abc.abstractmethod
+    def generate_distribution(self, *args: Any) -> npt.NDArray[Any]:
+        pass
+
+
+@dataclass
+class Poisson(Distribution):
+    rate: float
+
+    def generate_distribution(self, size: int) -> npt.NDArray[np.float64]:
+        logging.info(f"Generating Poisson distribution of size {size} with rate {self.rate}")
+        rval = np.zeros(size)
+        scale = 1 / self.rate
+        for i in range(1, size):
+            rval[i] = rval[i - 1] + np.random.exponential(scale)
+        return rval
+
+
+@dataclass
+class UniformInt(Distribution):
+    low: int
+    high: int
+
+    def generate_distribution(self, size: int) -> npt.NDArray[np.int64]:
+        logging.info(f"Generating uniform int distribution of size {size} with low {self.low} and high {self.high}")
+        return np.random.randint(self.low, self.high, size)  # high is exclusive
+
+
+@dataclass
+class NormalInt(Distribution):
+    mean: float
+    std: float
+
+    def generate_distribution(self, size: int) -> npt.NDArray[np.int64]:
+        logging.info(f"Generating normal int distribution of size {size} with mean {self.mean} and std {self.std}")
+        return np.random.normal(self.mean, self.std, size).astype(int)
+
+
+@dataclass
+class Same(Distribution):
+    start: float
+
+    def generate_distribution(self, size: int) -> npt.NDArray[np.float64]:
+        logging.info(f"Generating same distribution of size {size} with start {self.start}")
+        return np.ones(size) * self.start
+
+
+@dataclass
+class Even(Distribution):
+    rate: float
+
+    def generate_distribution(self, size: int) -> npt.NDArray[np.float64]:
+        logging.info(f"Generating even distribution of size {size} with rate {self.rate}")
+        return np.linspace(0, (size - 1) / self.rate, num=size)
+
+
+@dataclass
+class AdjustedUniformInt(Distribution):
+    low: int
+    high: int
+
+    def generate_distribution(self, lengths: npt.NDArray[np.int64]) -> npt.NDArray[np.float64]:
+        logging.info(f"Generating adjusted uniform int distribution with low {self.low} and high {self.high}")
+        rval = np.empty(len(lengths))
+        for i, length in enumerate(lengths):
+            rval[i] = np.random.randint(self.low, self.high - length[i])
+        return rval
