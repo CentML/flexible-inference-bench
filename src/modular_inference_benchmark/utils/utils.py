c0e6c85ac2a523f94037ff92a3781ea78d72527c
John Calderon <john.calderon@centml.ai>
2024-05-08 13:07:43 -0400

    made changes to pass validation from mypy

--- src/modular_inference_benchmark/utils/utils.py
+++ src/modular_inference_benchmark/utils/utils.py
@@ -5,10 +5,9 @@ logger = logging.getLogger(__name__)
 
 
 def configure_logging(args: argparse.Namespace) -> None:
-    kwargs = {
-        "format": "%(asctime)s %(levelname)-8s %(message)s",
-        "datefmt": "%Y-%m-%d %H:%M",
-        "level": logging.DEBUG if args.debug else logging.INFO,
-    }
 
-    logging.basicConfig(**kwargs)  # type: ignore
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M",
+        level=logging.DEBUG if args.debug else logging.INFO,
+    )
