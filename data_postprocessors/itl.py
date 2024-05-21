d8d64ce929b72150379aa2d3eb2568e986f5d3f6
Murali Andoorveedu <37849411+andoorve@users.noreply.github.com>
2024-05-21 08:21:33 -0700

    Add example itl postprocessor (#25)
    Signed-off-by: Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>

---
+++ data_postprocessors/itl.py
@@ -0,0 +1,37 @@
+"""
+Simple example of a data postprocessor script with minimal error checking and typing that shows a plot of ITLs.
+"""
+
+import json
+import argparse
+import matplotlib.pyplot as plt
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datapath", type=str, required=True, help="Path to the data file")
+    parser.add_argument("--output", type=str, required=False, help="Path to save the plot")
+    parser.add_argument('--request-num', type=int, default=0, help='Request number to plot')
+    return parser.parse_args()
+
+
+def plot_itl(data, idx, output):
+    itls = data[idx]["itl"]
+    plt.title(f"Inter-Token Latencies for Request {idx}")
+    plt.xlabel("Token")
+    plt.ylabel("Inter-Token Latency (s)")
+    plt.scatter(list(range(len(itls))), itls)
+    plt.show()
+    if output:
+        plt.savefig(output)
+
+
+def main():
+    args = parse_args()
+    with open(args.datapath, 'r') as f:
+        data = json.load(f)
+    plot_itl(data, args.request_num, args.output)
+
+
+if __name__ == "__main__":
+    main()
