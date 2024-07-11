8a5610f8cabc71c71e48a9512729f0ec83fa2007
John Calderon <john.calderon@centml.ai>
2024-07-11 10:29:55 -0400

    removed distribution from workloads and fix format

--- src/flexible_inference_benchmark/engine/workloads.py
+++ src/flexible_inference_benchmark/engine/workloads.py
@@ -12,26 +12,22 @@ class BaseWorkload(BaseModel):
 
 
 class General(BaseWorkload):
-    request_distribution: List[Union[str, int]] = ['exponential', 2]
     input_token_distribution: List[Union[str, int]] = ['uniform', 0, 255]
     output_token_distribution: List[Union[str, int]] = ['normal', 255, 2]
 
 
 class Summary(BaseWorkload):
-    request_distribution: List[Union[str, int]] = ['exponential', 2]
     input_token_distribution: List[Union[str, int]] = ['uniform', 800, 1000]
     output_token_distribution: List[Union[str, int]] = ['normal', 500, 2]
 
 
 class Story(BaseWorkload):
-    request_distribution: List[Union[str, int]] = ['exponential', 2]
     input_token_distribution: List[Union[str, int]] = ['uniform', 200, 300]
     output_token_distribution: List[Union[str, int]] = ['normal', 1000, 20]
 
 
 class Rag(BaseWorkload):
-    request_distribution: List[Union[str, int]] = ['exponential', 2]
-    input_token_distribution: List[Union[str, int]] = ['uniform', 400, 500]
+    input_token_distribution: List[Union[str, int]] = ['uniform', 4000, 2000]
     output_token_distribution: List[Union[str, int]] = ['normal', 1000, 5]
 
 
