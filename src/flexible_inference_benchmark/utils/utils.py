07fd5106f83c70db8c620f306fe1b37c490fa600
Benjamin Chislett <benjamin.chislett@epfl.ch>
2024-12-16 21:44:00 +0000

    safer "configure_logging" function

--- src/flexible_inference_benchmark/utils/utils.py
+++ src/flexible_inference_benchmark/utils/utils.py
@@ -5,9 +5,10 @@ logger = logging.getLogger(__name__)
 
 
 def configure_logging(args: argparse.Namespace) -> None:
+    show_debug_logs = hasattr(args, "debug") and args.debug
 
     logging.basicConfig(
         format="%(asctime)s %(levelname)-8s %(message)s",
         datefmt="%Y-%m-%d %H:%M",
-        level=logging.DEBUG if args.debug else logging.INFO,
+        level=logging.DEBUG if show_debug_logs else logging.INFO,
     )
