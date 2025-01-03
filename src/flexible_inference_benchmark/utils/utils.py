0bb4451a5a21b2e188837159a98bf3e1d8f18f87
Benjamin Chislett <chislett.ben@gmail.com>
2025-01-03 14:15:52 -0500

    typing and formatting fix

--- src/flexible_inference_benchmark/utils/utils.py
+++ src/flexible_inference_benchmark/utils/utils.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import logging
 import argparse
 import resource
@@ -13,6 +15,7 @@ def configure_logging(args: argparse.Namespace) -> None:
         level=logging.DEBUG if args.debug else logging.INFO,
     )
 
+
 # adapted from: https://github.com/vllm-project/vllm/blob/e1a5c2f0a123835558b1b1c9895181161527c55e/vllm/utils.py#L1857
 def set_max_open_files(n: Optional[int]) -> None:
     """Try to expand the system limit on the maximum number of open files."""
@@ -20,7 +23,8 @@ def set_max_open_files(n: Optional[int]) -> None:
     if n is not None and n > hard:
         logger.warning(
             "Number of requests exceeds maximum ulimit for number of open files"
-            "Consider increasing the limit with ulimit -n")
+            "Consider increasing the limit with ulimit -n"
+        )
     target_amount = n or 65535
     request_amount = max(soft, min(hard, target_amount))
     if request_amount == soft:
@@ -34,4 +38,8 @@ def set_max_open_files(n: Optional[int]) -> None:
             "Failed to increase ulimit from %s to %s."
             "This can lead to errors for large amounts of requests."
             "Consider increasing the limit with ulimit -n."
-            "Error: %s", soft, request_amount, e)
+            "Error: %s",
+            soft,
+            request_amount,
+            e,
+        )
