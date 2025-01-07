c5e6a2fa359e2199c4e07c1128d9b4a0a1066f04
Xin Li <xin@centml.ai>
2025-01-07 09:42:47 -0500

    clean up

--- examples/benchmark-serverless.sh
+++ examples/benchmark-serverless.sh
@@ -11,16 +11,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ulimit -n 100000
 # Make sure OpenAI API is present
 if [ -z "${OPENAI_API_KEY}" ]; then
-    echo "OPENAI_API_KEY is not set, please set it to use CentML Platform's OpenAI Compatible API endpoint"
+    echo "Environment variable OPENAI_API_KEY is not set, please set it to use CentML Platform's OpenAI Compatible API endpoint"
+    echo "Obtain your API key following this URL: https://app.centml.com/user/credentials"
     exit -1 
 fi
 
-for rate in 0.1 0.2 0.5 1 2 5 
+for rate in 0.2 0.5 1 2 5 10
 do
-    if (( $(echo "$rate >= 1" | bc -l) )); then
-        num_of_req=1000
+    if (( $(echo "$rate >= 2" | bc -l) )); then
+        num_of_req=500
     else
-        num_of_req=200
+        num_of_req=100
     fi
     echo "Starting benchmark with request rate: $rate for $num_of_req requests"
     datafile=fib_bench_serverless_poisson_$rate.json
@@ -31,7 +32,11 @@ do
 	    --use-out-token-dist-sharegpt \
 	    --output-file $datafile \
 	    --num-of-req $num_of_req
+    if [ $? -ne 0 ]; then
+        echo "fib command failed, exiting benchmarking"
+        exit -1
+    fi
     
-    echo "Analysing benchmarking data at "
+    echo "Analysing benchmarking data at $datafile"
     fib analyse --datapath $datafile
 done
