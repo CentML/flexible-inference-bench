f37e877dd23d7c2eeecb47572fce3129d325335b
Xin Li <xin@centml.ai>
2025-01-07 09:20:36 -0500

    update script

--- examples/benchmark-serverless.sh
+++ examples/benchmark-serverless.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git checkout xinli/serverless
 pip install -e .
 
 if [ ! -f ShareGPT_V3_unfiltered_cleaned_split.json ]; then
@@ -9,13 +8,30 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for rate in 0.2 0.5 1 5 10 15 20 25 30
+ulimit -n 100000
+# Make sure OpenAI API is present
+if [ -z "${OPENAI_API_KEY}" ]; then
+    echo "OPENAI_API_KEY is not set, please set it to use CentML Platform's OpenAI Compatible API endpoint"
+    exit -1 
+fi
+
+for rate in 0.1 0.2 0.5 1 2 5 
 do
+    if (( $(echo "$rate >= 1" | bc -l) )); then
+        num_of_req=1000
+    else
+        num_of_req=200
+    fi
+    echo "Starting benchmark with request rate: $rate for $num_of_req requests"
+    datafile=fib_bench_serverless_poisson_$rate.json
     fib benchmark \
 	    --config-file $SCRIPT_DIR/benchmark-serverless.json \
 	    --dataset-pat ShareGPT_V3_unfiltered_cleaned_split.json \
 	    --request-distribution poisson $rate \
 	    --use-out-token-dist-sharegpt \
-	    --output-file fib_bench_serverless_poisson_$rate.json \
-	    --num-of-req 200
+	    --output-file $datafile \
+	    --num-of-req $num_of_req
+    
+    echo "Analysing benchmarking data at "
+    fib analyse --datapath $datafile
 done
